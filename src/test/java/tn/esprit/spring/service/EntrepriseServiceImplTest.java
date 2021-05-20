657a0a61d7f9c579ebd2b46675b5ab08246cf8a3
Mahdi LABIDI <mahdi.labidi1@esprit.tn>
2021-05-20 21:38:34 +0100

    created tests

---
+++ src/test/java/tn/esprit/spring/service/EntrepriseServiceImplTest.java
@@ -0,0 +1,42 @@
+package tn.esprit.spring.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tn.esprit.spring.services.IEntrepriseService;
+
+public class EntrepriseServiceImplTest {
+
+	
+	@Autowired
+	IEntrepriseService es; 
+	
+	
+	@Test
+	public void testAjouterEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieveAllEntreprises() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieveEntreprise() {
+		fail("Not yet implemented");
+	}
+
+}
